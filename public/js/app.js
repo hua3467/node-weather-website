88a98d6e68f620622d457209bd87af8b03fc7d38
Aaron Yang <zhenhuayang@outlook.com>
2021-02-01 23:39:48 -0600

    update

--- public/js/app.js
+++ public/js/app.js
@@ -35,3 +35,27 @@ function getLocation() {
         const longitude = position.coords.longitude;
     }
 }
+
+const forecast = (latitude, longtitude, callback) => {
+
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=db52ff1d1ebc58e384ae214ed9750890`
+    
+    request({ url, json: true}, (error, { body }) => {
+        if (error) {
+            callback("Unable to connect to weather server.", undefined)
+        } else if (body.error) {
+            callback("Unable to find the location.", undefined);
+        } else {
+            // callback(undefined, `The weather is ${body.weather[0].main}. It feels like ${body.main.feels_like} degrees`);
+            callback(undefined, {
+                mainTemp: KtoF(body.main.temp),
+                feelsLike: KtoF(body.main.feel_like),
+                description: body.weather[0].description,
+                humidity: body.main.humidity,
+                pressure: body.main.pressure,
+                windSpeed: body.wind.speed,
+                windDegree: body.wind.deg
+            });
+        }
+    });
+}
